57de97d21eaeb68eceaad5d0a05455b984b9c308
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-04-16 21:08:42 -0400

    intro to the dom day 1

---
+++ module2/total-recall/functions.js
@@ -0,0 +1,57 @@
+const submissions = [
+    {
+      learner_id: 125,
+      assignment_id: 1,
+      submission: {
+        submitted_at: "2023-01-25",
+        score: 47
+      }
+    },
+    {
+      learner_id: 125,
+      assignment_id: 2,
+      submission: {
+        submitted_at: "2023-02-12",
+        score: 150
+      }
+    },
+    {
+      learner_id: 125,
+      assignment_id: 3,
+      submission: {
+        submitted_at: "2023-01-25",
+        score: 400
+      }
+    },
+    {
+      learner_id: 132,
+      assignment_id: 1,
+      submission: {
+        submitted_at: "2023-01-24",
+        score: 39
+      }
+    },
+    {
+      learner_id: 132,
+      assignment_id: 2,
+      submission: {
+        submitted_at: "2023-03-07",
+        score: 140
+      }
+    }
+  ];
+
+// Date
+  const currentDate = new Date();
+  console.log(currentDate.getDay());
+
+  const date = new Date("2023-01-25");
+  console.log(date.getDay());
+
+
+  // forEach and find
+
+  submissions.forEach(s => console.log(s.submission.score));
+
+  const result = submissions.find(el => el.assignment_id === 3);
+  console.log(result);
