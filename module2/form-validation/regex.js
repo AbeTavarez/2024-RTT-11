527d708f19f8f3692b1ba091cbe19f109a69fc51
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-04-24 16:04:10 -0400

    form validation

---
+++ module2/form-validation/regex.js
@@ -0,0 +1,12 @@
+const str = 'All programmers start with a hello World application!';
+
+const regex1 = /hello/ig;
+
+// Regex Methods
+console.log(regex1.test(str)); // return true or false if matched
+console.log(regex1.exec(str)); //
+
+// String method
+console.log(str.match(regex1)); // returns array of matches
+console.log(str.search(regex1)); // returns the index
+console.log(str.replace(regex1, 'Hi')); // find and replace
